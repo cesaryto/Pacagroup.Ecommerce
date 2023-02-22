ac46f1576fb18184e0b8b183ed9f5b48ef5ece7c
cesar augusto calderon <cesar.calderon@itglobers.com>
2023-02-21 22:30:14 -0500

    Agregar archivos de proyecto.

---
+++ Pacagroup.Ecommerce.Infraestructure.Interface/ICustomersRepository.cs
@@ -0,0 +1,28 @@
+﻿using Pacagroup.Ecommerce.Domain.Entity;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Pacagroup.Ecommerce.Infraestructure.Interface
+{
+    #region Métodos síncronos
+    public interface ICustomersRepository
+    {
+        bool Insert(Customers customer);
+        bool Update(Customers customer);
+        bool Delete(string customerId);
+        Customers Get(string customerId);
+        IEnumerable<Customers> GetAll();
+
+        #endregion
+
+        #region Métodos Asíncronos
+
+        Task<bool> InsertAsync(Customers customer);
+        Task<bool> UpdateAsync(Customers customer);
+        Task<bool> DeleteAsync(string customerId);
+        Task<Customers> GetAsync(string customerId);
+        Task<IEnumerable<Customers>> GetAllAsync();
+
+        #endregion
+    }
+}
